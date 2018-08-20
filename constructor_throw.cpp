93e429398d09273590871500c55bafce0d45aad5
kevinwan1991 <kevinwan1991@gmail.com>
2018-08-20 16:04:25 -0400

    Create constructor_throw.cpp

---
+++ constructor_throw.cpp
@@ -0,0 +1,33 @@
+// Example program
+#include <iostream>
+#include <string>
+
+struct B
+{
+	B(){ std::cout << "B()" << std::endl; }
+	~B(){ std::cout << "~B()" << std::endl; }
+};
+
+struct C
+{
+	C(){ std::cout << "C()" << std::endl; throw std::exception(); }
+	~C(){ std::cout << "~C()" << std::endl; }
+};
+
+struct A
+{
+	B b;
+	C c;
+	A():b(B())
+	{
+		std::cout << "A()" << std::endl;
+		c = C();
+		throw std::exception();
+	}
+	~A(){ std::cout << "~A()" << std::endl; }
+};
+
+int main()
+{
+	A a;
+}
